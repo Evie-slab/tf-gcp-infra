8783c89817fa7dea65510348b114ff6da732bcf2
ZhengZhangXiao <zheng.yang4@northeastern.edu>
2024-02-21 18:08:33 -0800

    assignment3-1

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("/Users/eviezheng/Desktop/google cloud key/cloud-assignment-0101-30f6a75f253b.json")
+  //credentials = file("/Users/eviezheng/Desktop/google cloud key/cloud-assignment-0101-30f6a75f253b.json")
   project     = "cloud-assignment-0101"
   region        = "us-east1"
 }
